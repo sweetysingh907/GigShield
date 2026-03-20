77ec5405f2f86115790ee4b32596db134eb73767
SWEETY SINGH <nahariyasweety@gmail.com>
2026-03-20 11:27:35 +0530

    Refactor checkWeather function for clarity

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 async function checkWeather() {
   const city = document.getElementById("city").value;
-  const apiKey = "000946666df88e700bad2bf38e6d4062"; // 🔥 paste your key here
+  const apiKey = "000946666df88e700bad2bf38e6d4062"; //  paste your key here
 
   if (city === "") {
-    document.getElementById("result").innerText = "⚠️ Please enter a city";
+    document.getElementById("result").innerText = " Please enter a city";
     return;
   }
 
-  document.getElementById("result").innerText = "⏳ Checking risk...";
+  document.getElementById("result").innerText = " Checking risk...";
 
   const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
@@ -19,7 +19,7 @@ async function checkWeather() {
 
     if (data.cod !== 200) {
       document.getElementById("result").innerText =
-        "❌ City not found";
+        "City not found";
       return;
     }
 
